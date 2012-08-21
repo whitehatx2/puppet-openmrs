052f903ef58b9418e3ddc6869b798368f5652680
Svend Sorensen <svends@uw.edu>
2012-08-21 12:58:14 -0700

    Unzip openmrs war

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,4 +4,10 @@ class openmrs {
     creates => '/usr/src/openmrs.war',
     command => '/usr/bin/wget \'http://iweb.dl.sourceforge.net/project/openmrs/releases/OpenMRS_1.9.1/openmrs.war\''
   }
+
+  exec { 'unzip-openmrs':
+    command => '/usr/bin/unzip /usr/src/openmrs.war -d /var/lib/tomcat6/webapps/openmrs',
+    creates => '/var/lib/tomcat6/webapps/openmrs',
+    require => Exec['download-openmrs'],
+  }
 }
