ff14daa2b3e59984da5c692134cb76e569184970
aniksing <aniksing@ITECH.(none)>
2013-02-20 23:52:31 +0300

    timeout for data import process set

--- manifests/init.pp
+++ manifests/init.pp
@@ -137,12 +137,13 @@ class openmrs{
     cwd => '/usr/src',
     command => '/usr/bin/wget \'https://www.dropbox.com/s/lnfvd9r7cblpawr/kenyaemr-concepts-2013.1.sql\'',
     creates => '/usr/src/kenyaemr-concepts-2013.1.sql',
-    timeout => -1,
+    timeout => 5000,
+  }
 
   exec { "apply-concept-dictionary":
     cwd => '/usr/src',
     command => '/usr/bin/mysql openmrs < kenyaemr-concepts-2013.1.sql',
-    timeout => -1,
+    timeout => 5000,	
   }
 
   notify {"OpenMRS-8":
